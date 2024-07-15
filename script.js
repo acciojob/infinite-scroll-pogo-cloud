204b09776a548499db7f618a0c5cd85983f36ab0
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-07-15 10:16:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//your code here!
+let lists=document.querySelector("#infi-list");
+let j=1;
+for(let i=0;i<10;i++){
+	let listItem=document.createElement("li");
+	listItem.setAttribute("class","my-list");
+	listItem.textContent=`Item +${j++}`;
+	lists.appendChild(listItem);
+}
+lists.addEventListener("scroll",(event)=>{
+	if(lists.scrollTop+lists.clientHeight>=lists.scrollHeight){
+		let currLen=documentary.querySelector("#infi-list").length;
+		for(let i=currLen;i<currLen+2;i++){
+			let listItem=document.createElement("li");
+	        listItem.setAttritbute("class","my-list");
+	        listItem.textContent=`Item +${j++}`;
+	         lists.appendChild(listItem);
+			
+		}
+	}
+})
 
